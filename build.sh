054d1b84ae3137bab77ab5e1acf7407786dd1aff
sarenord <ajsheld12@gmail.com>
2020-11-28 23:40:06 -0500

    fixed polybar build process

--- build.sh
+++ build.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/bash
 
 _buildsh_path="$(realpath -- "$0")"
+_base_dir=$(dirname $_buildsh_path)
 
 function build_polybar() {
-	[[ ! -d ./work ]] && mkdir work
-	cd work
-	git clone http://github.com/polybar/polybar
-	cd polybar
-	git submodule update --init --recursive
-	mkdir build
-	cd build
-	cmake ..
-	make -j $(nproc)
-	cd ../../..
-	rm -rf work/polybar
+    git clone git@github.com:polybar/polybar airootfs/usr/bin/polybar
+    cd airootfs/usr/bin/polybar
+    git submodule update --init --recursive
+    mkdir build
+    cd build
+    cmake ..
+    make -j $(nproc)
+    cd ../..
+    mv polybar polybar-build
+    cp polybar-build/build/bin/polybar polybar-build/build/bin/polybar-msg .
+    rm -rf polybar-build
+    cd $_base_dir
 }
 
-function get_dotfiles() {
-	cp -r /home/sarenord/Documents/Storage/Git/dotfiles/* /home/sarenord/Documents/Storage/Git/dotfiles/.* airootfs/home/sarenord/
-	# rm -rf airootfs/home/sarenord/.* airootfs/home/sarenord/*; git clone git@github.com:sarenord/dotfiles.git airootfs/home/sarenord
+function build_stterm() {
+    git clone git@github.com:shell-drick/stterm airootfs/usr/bin/stterm
+    cd airootfs/usr/bin/stterm
+    make
+    cp st ..
+    cd ..
+    rm -rf stterm
+    cd $_base_dir
 }
+
 
 function build_deps() {
-	get_dotfiles
-	build_polybar
+    rm -rf airootfs/home/sarenord airootfs/usr/bin/*
+    git clone git@github.com:shell-drick/dotfiles airootfs/home/sarenord
+    build_stterm
+    build_polybar
 }
 
 [[ "$*" == *'--deps'* ]] && build_deps
