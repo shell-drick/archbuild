152dc1ac2090379625c783de0e33a7e227289fac
sarenord <ajsheld12@gmail.com>
2021-03-19 22:47:47 -0400

    temp fixed permissions issue with git repos

--- profiledef.sh
+++ profiledef.sh
@@ -10,3 +10,4 @@ install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+file_permissions=(["/usr/bin/polybar"]="0:0:755" ["/usr/bin/polybar-msg"]="0:0:755" ["/usr/bin/st"]="0:0:755")
